01e68a534d7e246719068a1dd5a43139d3653ff2
Dmitry <recfrag@gmail.com>
2021-02-21 13:47:24 +0300

    Added record field, fixed intervals

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-let cvs = document.getElementById("canvas");
-let ctx = cvs.getContext("2d");
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
 canvas.width =
   document.documentElement.clientWidth / 1.5 > 600
     ? 600
@@ -11,23 +11,27 @@ canvas.height =
 
 let scoresCounter = document.getElementById('score');
 let levelCounter = document.getElementById('level');
+let record = document.getElementById('record');
 let width = cvs.width;
 let height = cvs.height;
-let bg = new Image();
-let car = new Image();
-let enemy = new Image();
+const bg = new Image();
+const car = new Image();
+const enemy = new Image();
 let speed = 7;
 let xCar = width / 2 - 25;
 let yCar = height - 50;
 let scores = 0;
 let enemiesSpd = 3;
 let level=1;
-let intervalReaspawnEnemies = 500;
-let intervalToNextLevel = 4000;
+let intervalReaspawnEnemies = 45;
+let intervalToNextLevel = 500;
+let enemies = [];
 
 bg.src = "img/bg.png";
 car.src = "img/c.png";
 enemy.src = "img/e.png";
+
+record.innerHTML = localStorage.getItem('record') ? localStorage.getItem('record') : 'no record';
 
 let keys = {
   W: 87,
@@ -65,8 +69,6 @@ let move = function () {
   if (isKeyDown("RIGHT") && xCar < width - 50) xCar += speed;
 };
 
-let enemies = [];
-
 enemies[0] = {
   xE: Math.abs(Math.floor(Math.random() * width - 50)),
   yE: -50
@@ -74,12 +76,17 @@ enemies[0] = {
 
 function draw() {
   scores++;
-  if(scores !== 0 && scores % 35 === 0) {
+  if(scores !== 0 && scores % intervalReaspawnEnemies === 0) {
     enemies.push({
       xE: Math.abs(Math.floor(Math.random() * width - 50)),
       yE: -40
     });
   }
+  if(scores % intervalToNextLevel === 0){
+      enemiesSpd += 0.5;
+      level++
+      intervalReaspawnEnemies -= 3;  
+    };
   scoresCounter.innerHTML = scores;
   levelCounter.innerHTML=level;
 
@@ -87,21 +94,18 @@ function draw() {
   ctx.drawImage(car, xCar, yCar, 50, 50);
   for (let i = 0; i < enemies.length; i++) {
     ctx.drawImage(enemy, enemies[i].xE, enemies[i].yE, 50, 50);
-    // if (scores % intervalToNextLevel === 0){
-    //   enemiesSpd += 1;
-    //   level++
-    //   intervalReaspawnEnemies-=20;  
-    // };
     enemies[i].yE += enemiesSpd;
     
     if (enemies[i].yE + 40 == yCar && enemies[i].xE + 40 <= xCar + 40 && xCar <= enemies[i].xE + 40 ||
       enemies[i].yE + 40 == yCar && enemies[i].xE + 40 >= xCar + 40 && enemies[i].xE <= xCar + 40 ||
       enemies[i].yE + 40 >= yCar && enemies[i].yE <= yCar + 40 && enemies[i].xE + 40 <= xCar + 40 && xCar <= enemies[i].xE + 40 ||
       enemies[i].yE + 40 >= yCar && enemies[i].yE <= yCar + 40 && xCar <= enemies[i].xE && enemies[i].xE <= xCar + 40) {
-
-      location.reload();
+        if(localStorage.getItem('record') && localStorage.getItem('record') < scores || !localStorage.getItem('record')) {
+          localStorage.setItem('record', scores)
+        }
+        location.reload();
     }
-  }
+  } 
 
   move();
 
